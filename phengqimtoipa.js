274ccd6c14e079e84fcbe3fb5a36391f8b84a26d
hhliow <hxliow@gmail.com>
2017-08-31 23:23:11 -0400

    Update phengqimtoipa.js

--- phengqimtoipa.js
+++ phengqimtoipa.js
@@ -1,6 +1,8 @@
 phengqimSyllableToIPA = function(s) {
   // TODO: assert s contains no space.
   
+  // Initials.
+  
   s = s.replace(/^gh/, 'ɦ');
   
   s = s.replace('ng', 'ŋ');
@@ -13,14 +15,23 @@ phengqimSyllableToIPA = function(s) {
 
   var apicalInitial = '^([dtsznʈɖʂʐɳ]|tsh?|dz|th|ʈh|ʈʂh?|ɖʐ)';
   var retroflexInitial = '^([ʈɖʂʐɳ]||ʈh|ʈʂh?|ɖʐ)';
-  s = s.replace(new RegExp('(' + retroflexInitial + ')' + 'r(w?[aeou])'), '$1ɣ$2');
   s = s.replace(new RegExp('(' + retroflexInitial + ')' + '[rɣ](w?[iyv])'), '$1$2');
   
   s = s.replace(/^nj/, 'ȵʑ');
-  s = s.replace(/^s[hc]/, 'ɕ');
   s = s.replace(/^t?c/, 'tɕ');
-  s = s.replace(/^([dz])c/, '$1ʑ');
+  s = s.replace(/^dc/, 'dʑ');
+  s = s.replace(/^s[hcj]/, 'ɕ');
+  s = s.replace(/^z[cj]/, 'ʑ');
   s = s.replace(/^([dn]?)zh/, '$1ʑ');
   
+  // Medials.
+
+  s = s.replace(new RegExp('(' + apicalInitial + ')' + 'r(w?[aeou])'), '$1ɣ$2');
+  s = s.replace(/yi/, 'wi');
+  
   return s;
+  
+  // Syllabics.
+  s = s.replace(/([^rɣji(([rɣj][wu])])a([^e]|[xh]|($))/, '$1ɑ$2');
+  
 }
