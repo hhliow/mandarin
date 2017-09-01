dcf1fe3e28e3e252f47d426d2d082540dcd25757
hhliow <hxliow@gmail.com>
2017-08-31 23:04:11 -0400

    Update phengqimtoipa.js

--- phengqimtoipa.js
+++ phengqimtoipa.js
@@ -1,20 +1,26 @@
 phengqimSyllableToIPA = function(s) {
   // TODO: assert s contains no space.
-  s.replace('ng', 'ŋ');
-  s.replace(/^gh/, 'ɦ');
   
-  s.replace(/^t(.?h?r)/, 'ʈ$1');
-  s.replace(/^d(.?r)/, 'ɖ$1');
-  s.replace(/^nr/, 'ɳ');
-  s.replace(/^(.?)sr/, '$1ʂr');
-  s.replace(/^(.?)zr/, '$1ʐr');
+  s = s.replace(/^gh/, 'ɦ');
+  
+  s = s.replace('ng', 'ŋ');
+  
+  s = s.replace(/^t(.?h?[rɣ])/, 'ʈ$1');
+  s = s.replace(/^d(.?[rɣ])/, 'ɖ$1');
+  s = s.replace(/^n([rɣ])/, 'ɳ$1');
+  s = s.replace(/^(.?)s([rɣ])/, '$1ʂ$2');
+  s = s.replace(/^(.?)z([rɣ])/, '$1ʐ$2');
 
   var apicalInitial = '^([dtsznʈɖʂʐɳ]|tsh?|dz|th|ʈh|ʈʂh?|ɖʐ)';
   var retroflexInitial = '^([ʈɖʂʐɳ]||ʈh|ʈʂh?|ɖʐ)';
-  s.replace(new RegExp(retroflexInitial + 'r(w?[aeou])'), '$1ɣ$2');
-  s.replace(new RegExp(retroflexInitial + 'r(w?[iyv])'), '$1$2');
-
-  s.replace(/^nj/, 'ȵʑ');
-  s.replace(/^([dz])c/, '$1ʑ');
-  s.replace(/^([dn]?)zh/, '$1ʑ');
+  s = s.replace(new RegExp('(' + retroflexInitial + ')' + 'r(w?[aeou])'), '$1ɣ$2');
+  s = s.replace(new RegExp('(' + retroflexInitial + ')' + '[rɣ](w?[iyv])'), '$1$2');
+  
+  s = s.replace(/^nj/, 'ȵʑ');
+  s = s.replace(/^s[hc]/, 'ɕ');
+  s = s.replace(/^t?c/, 'tɕ');
+  s = s.replace(/^([dz])c/, '$1ʑ');
+  s = s.replace(/^([dn]?)zh/, '$1ʑ');
+  
+  return s;
 }
