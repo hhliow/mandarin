65de12a627f15bb694d68e3378fc14a67662a208
hhliow <hxliow@gmail.com>
2017-09-07 23:00:22 -0400

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,10 +1,21 @@
 // Reads key-value pairs from a tab-separated string, where each line represents one key-value pair.
 readMapFromTsvString = function(s) {
-  var result = new Map();
+  var table = readArrayFromTsvString(s);
+  for (var i = 0; i < table.length; i++) {
+    var key = table[i][0];
+    if (/^\/.*\/$/.test(key)) {
+         key = new RegExp(key.replace(/^\/(.*)\/$/, '$1'));
+    }
+    result.set(key, table[i][1]);
+  }
+  return result;
+}
+
+readArrayFromTsvString = function(s) {
+  var result = [];
   var lines = s.match(/[^\r\n]+/g);
   for (var i = 0; i < lines.length; i++) {
-    var pair = lines[i].split('\t');
-    result.set(pair[0], pair[1]);
+    result.push(lines[i].split('\t'));
   }
   return result;
 }
@@ -12,10 +23,22 @@ readMapFromTsvString = function(s) {
 // Reads from a file at the specified url and push the content string to the array dest.
 function readStringFromFile(url, dest) {
   var x = new XMLHttpRequest();
-  x.open('GET', s);
+  x.open('GET', url);
   x.onload = function() {
     dest.push(x.responseText);
     setTimeout(readStringFromFile, 5000);
   };
   x.send();
+}
+
+applyRegexCumulativeLy = function(s, map) {
+  var result = '';
+  map.forEach(
+      function(value, key) {
+        if (key.test(s)) {
+          result += value;
+        }
+      }
+  ); 
+  return result;
 }
